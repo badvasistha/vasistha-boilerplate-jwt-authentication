13c0595c5c79f7b375d67acf10de579cc5639887
Badri Vasistha <badri.vasistha@gmail.com>
2019-07-25 15:16:09 -0700

    script dependencies, and api routes.

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const authRoutes = require('./authRoutes');
+
+// /api prepended to these routes
+
+router.use('/auth', authRoutes);
+
+module.exports = router;
