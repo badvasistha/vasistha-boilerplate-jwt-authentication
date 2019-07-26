53f13b0ba828d411fc97c9dd221d606caa83e58e
Badri Vasistha <badri.vasistha@gmail.com>
2019-07-26 10:51:31 -0700

    validate email for User model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
+const validateEmail = function(email){
+    return validator.isEmail(email);
+};
 
 const Schema = mongoose.Schema;
 
@@ -8,7 +12,11 @@ const UserSchema = new Schema({
         type: String,
         unique: true,
         required: true,
-        lowercase: true
+        lowercase: true, 
+        validate: [
+            validateEmail,
+            'Please enter a valid email address'
+        ]
     },
     password: {
         type: String,
