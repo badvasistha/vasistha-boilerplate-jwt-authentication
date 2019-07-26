1b66ca122cde79f3177bd2c7ab5017d9d67fda88
Badri Vasistha <badri.vasistha@gmail.com>
2019-07-26 11:21:30 -0700

    create signIn route lightly configure passport.

--- routes/apiRoutes/authRoutes.js
+++ routes/apiRoutes/authRoutes.js
@@ -4,5 +4,7 @@ const authController = require('./../../controllers/authController')
 router.route('/signup')
     .post(authController.signup);
     
+router.route('/signin')
+    .post(authController.signIn);
 
 module.exports = router;
