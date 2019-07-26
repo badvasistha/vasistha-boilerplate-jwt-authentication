5f32b7ea412e217e1ebfa98d7c0925a8482050a8
Badri Vasistha <badri.vasistha@gmail.com>
2019-07-26 14:39:58 -0700

    Finish backend setup for oauth

--- routes/apiRoutes/authRoutes.js
+++ routes/apiRoutes/authRoutes.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
+
 const authController = require('./../../controllers/authController')
+
+const passportService = require('./../../services/passport');
+const authMiddleware = require('./../../middlewares/authMiddlewares');
 
 router.route('/signup')
     .post(authController.signup);
     
 router.route('/signin')
-    .post(authController.signIn);
+    .post(authMiddleware.requireSignIn, authController.signIn);
 
 module.exports = router;
